b8967436b10b2dfcd1ff48376820772ee6770923
Nikolas Saf <n.saf@student.tugraz.at>
2021-04-14 16:18:43 +0200

    HIKER-06 create map activity [WM,SN]

--- app/src/main/java/com/team28/thehiker/MainActivity.kt
+++ app/src/main/java/com/team28/thehiker/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.team28.thehiker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, position_on_map::class.java)
+
+        startActivity(intent)
+
+
+
+
     }
 }
