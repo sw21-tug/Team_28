1f6523d2cd80fb237d9e9b4fd52a9f5d2fc918ad
TamaraSchweiger <tschweig@htl-steyr.ac.at>
2021-04-14 16:25:53 +0200

    HIKER-4 created basic layout and method placeholders for testing [ST,LT]

---
+++ app/src/main/java/com/team28/thehiker/AltitudeActivity.kt
@@ -0,0 +1,19 @@
+package com.team28.thehiker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class AltitudeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_altitude)
+
+
+    }
+
+    fun updateAltitude(altitude : Double){
+
+    }
+
+}
