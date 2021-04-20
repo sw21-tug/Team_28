57b6fe227f96006dcff4fafef03fb5a77d0efd3c
Sebastian Grief <sebastian.grief@student.tugraz.at>
2021-04-20 15:23:08 +0200

    HIKER-3: main activity layout and tests [DJ,GS,LJ]

--- app/src/androidTest/java/com/team28/thehiker/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/team28/thehiker/MainActivityInstrumentedTest.kt
@@ -3,18 +3,29 @@ package com.team28.thehiker
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.times
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityInstrumentedTest {
 
     @get:Rule
-    var activityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+    var activityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule<MainActivity>(MainActivity::class.java)
+
+    @Before
+    fun setUp() {
+        Intents.init()
+    }
 
     @Test
     fun button_altitude() {
@@ -35,16 +46,25 @@ class MainActivityInstrumentedTest {
     }
 
     @Test
-    fun check_listview_containItems() {
+    fun check_listview_containsItems() {
         onView(withId(R.id.scrollview_menu))
             .check(matches(hasDescendant(withId(R.id.btn_altitude))))
+
+        onView(withId(R.id.scrollview_menu))
+                .check(matches(hasDescendant(withId(R.id.btn_position_on_map))))
     }
 
     @Test
-    fun check_listview_containItems() {
-        onView(withId(R.id.scrollview_menu))
-            .check(matches(hasDescendant(withId(R.id.btn_position_on_map))))
+    fun onButtonClick_correctActivityStarted() {
+        onView(withId(R.id.btn_altitude))
+            .perform(click())
+
+        //TODO: this can be changed to real activities when implemented
+        Intents.intended(hasComponent(TestActivity::class.java.name), times(1))
     }
 
-
+    @After
+    fun cleanUp() {
+        Intents.release()
+    }
 }
--- app/src/test/java/com/team28/thehiker/MainActivityTest.kt
+++ app/src/test/java/com/team28/thehiker/MainActivityTest.kt
@@ -1,8 +1,29 @@
 package com.team28.thehiker
 
-import org.junit.Rule
+import com.team28.thehiker.Permissions.PermissionHandler
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 
 class MainActivityTest {
 
+    @Mock
+    var permissionHandlerMock = PermissionHandler()
+
+    @Test
+    fun testPermissions_checkPermissionsIfNotGranted_PermissionsGranted() {
+        `when`(permissionHandlerMock.checkPermissionsGranted())
+                .thenReturn(false)
+
+        Mockito.verify(permissionHandlerMock.askUserForPermissions(), Mockito.times(1))
+    }
+
+    @Test
+    fun testPermissions_checkPermissionsIfNotGranted_PermissionsNotGranted() {
+        `when`(permissionHandlerMock.checkPermissionsGranted())
+                .thenReturn(true)
+
+        Mockito.verify(permissionHandlerMock.askUserForPermissions(), Mockito.times(0))
+    }
 }
