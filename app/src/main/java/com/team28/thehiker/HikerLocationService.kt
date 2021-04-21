356457d950af34639f09aed50537c0719dffca32
Thomas L <tomlangs13@gmail.com>
2021-04-21 15:48:49 +0200

    HIKER-17 implemented location updates [ST,LT]

--- app/src/main/java/com/team28/thehiker/HikerLocationService.kt
+++ app/src/main/java/com/team28/thehiker/HikerLocationService.kt
@@ -1,18 +1,32 @@
 package com.team28.thehiker
 
+import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import android.os.Looper
 import android.util.Log
-import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.*
 
 class HikerLocationService : Service() {
 
     private val binder = HikerLocationBinder()
     private lateinit var locationProvider: FusedLocationProviderClient
+    private var locationCallback = LocationCallbackHandler()
+    private var callbackList = arrayListOf<HikerLocationCallback>()
 
+
+    @SuppressLint("MissingPermission") // not checked because it will be checked by the MainActivity
     override fun onBind(intent: Intent): IBinder {
+        locationProvider = LocationServices.getFusedLocationProviderClient(this)
+
+        val locationRequest = LocationRequest.create()
+        locationRequest.interval = 2500
+        locationRequest.fastestInterval = 1000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        locationProvider.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
         return binder
     }
 
@@ -25,7 +39,13 @@ class HikerLocationService : Service() {
     }
 
     fun addLocationCallback(locationCallback: HikerLocationCallback) {
+        callbackList.add(locationCallback)
+    }
 
+    inner class LocationCallbackHandler : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            callbackList.forEach { it.notifyLocationUpdate() }
+        }
     }
 
     inner class HikerLocationBinder : Binder() {
