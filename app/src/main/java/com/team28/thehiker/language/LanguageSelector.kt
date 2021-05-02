375d09665b9ae2ee4fc82dedd06cfcd3c8d38d08
Thomas L <tomlangs13@gmail.com>
2021-05-02 18:24:12 +0200

    HIKER-29 Implemented language switch to english function  [WM,LT]

--- app/src/main/java/com/team28/thehiker/language/LanguageSelector.kt
+++ app/src/main/java/com/team28/thehiker/language/LanguageSelector.kt
@@ -3,25 +3,28 @@ package com.team28.thehiker.language
 import android.content.Context
 import android.content.res.Configuration
 import android.os.Build
-import androidx.annotation.RequiresApi
 import java.util.*
 
 class LanguageSelector {
 
     companion object {
-        fun setLocaleToRussian(context: Context){
-            val locale = Locale("ru", "RU")
+        fun setLocaleToRussian(context: Context) {
+            setLocale(context, Locale("ru", "RU"))
+        }
 
+
+        fun setLocaleToEnglish(context: Context) {
+            setLocale(context, Locale("en", "EN"))
+        }
+
+        private fun setLocale(context: Context, locale: Locale) {
             val config = Configuration(context.resources.configuration)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
                 config.setLocale(locale)
             } else {
                 config.locale = locale
             }
-
             context.resources.updateConfiguration(config, context.resources.displayMetrics)
-        }
-        fun setLocaleToEnglish(context: Context){
         }
     }
 }
