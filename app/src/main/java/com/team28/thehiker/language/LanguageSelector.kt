2e3d6886c6f81c108e99ccb1df4475314434752a
Markus Watko <markus.watko@tugraz.at>
2021-05-02 18:05:45 +0200

    HIKER-29 Implemented language switch to russian function  [WM,LT]

--- app/src/main/java/com/team28/thehiker/language/LanguageSelector.kt
+++ app/src/main/java/com/team28/thehiker/language/LanguageSelector.kt
@@ -1,12 +1,25 @@
 package com.team28.thehiker.language
 
 import android.content.Context
+import android.content.res.Configuration
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.util.*
 
 class LanguageSelector {
 
     companion object {
         fun setLocaleToRussian(context: Context){
+            val locale = Locale("ru", "RU")
+
+            val config = Configuration(context.resources.configuration)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                config.setLocale(locale)
+            } else {
+                config.locale = locale
+            }
+
+            context.resources.updateConfiguration(config, context.resources.displayMetrics)
         }
     }
-
 }
