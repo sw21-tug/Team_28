7d1f64a9e3c5400a4a0c480b22a7498a17be116a
Lukas Feldgrill <feldgrill@student.tugraz.at>
2021-05-04 15:56:46 +0200

    HIKER-30 implemented preference handler [FL,GS]

--- app/src/main/java/com/team28/thehiker/SharedPreferenceHandler/ISharedPreferenceHandler.kt
+++ app/src/main/java/com/team28/thehiker/SharedPreferenceHandler/ISharedPreferenceHandler.kt
@@ -1,10 +1,11 @@
 package com.team28.thehiker.SharedPreferenceHandler
 
+import android.app.Activity
 import android.content.Context
 
 interface ISharedPreferenceHandler {
 
-    fun getLocalizationString(context: Context) : String
+    fun getLocalizationString(context: Activity) : String?
 
-    fun setLocalizationString(context: Context, localizationString: String)
+    fun setLocalizationString(context: Activity, localizationString: String)
 }
