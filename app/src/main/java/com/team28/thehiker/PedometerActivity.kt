00faa4a39491797a8b4a78d282efec1e73dafde6
Jaime993155 <33482887+Jaime993155@users.noreply.github.com>
2021-05-05 14:38:42 +0200

    HIKER-13 implemented detector activity not working[SM,NJ]

--- app/src/main/java/com/team28/thehiker/PedometerActivity.kt
+++ app/src/main/java/com/team28/thehiker/PedometerActivity.kt
@@ -1,5 +1,6 @@
 package com.team28.thehiker
 
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -7,13 +8,24 @@ import android.hardware.SensorManager
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.getSystemService
 
 
 class PedometerActivity  : AppCompatActivity(), SensorEventListener {
+   private lateinit var sensorManager : SensorManager
+    private lateinit var stepSensor : Sensor
+    private var sensorPresent : Boolean = false
+    private lateinit var stepsTaken : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pedometer)
         supportActionBar?.hide()
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+       if( sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)!=null){
+           stepSensor=sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
+           sensorPresent = true
+       }
+        stepsTaken  = findViewById<TextView>(R.id.txtViewSteps).text as String
     }
 
     fun updateStepCounter (steps : Int){
@@ -22,11 +34,15 @@ class PedometerActivity  : AppCompatActivity(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        event?.values?.get(0)?.let { updateStepCounter(it.toInt()) }
+        stepsTaken = findViewById<TextView>(R.id.txtViewSteps).text as String
+        var stepCounter : Int = stepsTaken.toInt()
+        if (event!= null){
+            updateStepCounter(stepCounter++)
+        }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        TODO("Not yet implemented")
+
     }
 
 
