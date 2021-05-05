c33e8006ebef2d652663ab3dc7632f4e07738bdf
Maria Seiser <seiser@student.tugraz.at>
2021-05-05 15:02:37 +0200

    HIKER-13 works like detector now [SM,NJ]

--- app/src/main/java/com/team28/thehiker/PedometerActivity.kt
+++ app/src/main/java/com/team28/thehiker/PedometerActivity.kt
@@ -1,6 +1,7 @@
 package com.team28.thehiker
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -9,41 +10,49 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.getSystemService
+import kotlinx.coroutines.Delay
 
 
 class PedometerActivity  : AppCompatActivity(), SensorEventListener {
-   private lateinit var sensorManager : SensorManager
+    private lateinit var sensorManager : SensorManager
     private lateinit var stepSensor : Sensor
     private var sensorPresent : Boolean = false
-    private lateinit var stepsTaken : String
+    private var stepsTaken : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pedometer)
         supportActionBar?.hide()
+
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-       if( sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)!=null){
-           stepSensor=sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
-           sensorPresent = true
-       }
-        stepsTaken  = findViewById<TextView>(R.id.txtViewSteps).text as String
+
+        checkStepSensorAvailable()
+
+        if(sensorPresent)
+            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_NORMAL)
+
+    }
+
+    fun checkStepSensorAvailable(){
+        if( sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)!=null) {
+            stepSensor=sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
+            sensorPresent = true
+        }
     }
 
     fun updateStepCounter (steps : Int){
         val steps_text = findViewById<TextView>(R.id.txtViewSteps)
-        steps_text.text= steps.toString()
+        steps_text.text = steps.toString()
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        stepsTaken = findViewById<TextView>(R.id.txtViewSteps).text as String
-        var stepCounter : Int = stepsTaken.toInt()
         if (event!= null){
-            updateStepCounter(stepCounter++)
+            updateStepCounter(++stepsTaken)
         }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
 
     }
-
 
 }
