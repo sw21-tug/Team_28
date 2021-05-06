7cbb74cf8c3e1ce7e1fc52e6f8a520bccc3b07c3
Thomas L <tomlangs13@gmail.com>
2021-05-06 09:40:26 +0200

    HIKER-36 Now testing to ensure the language stays set after an app restart [FL,LT]

--- app/src/androidTest/java/com/team28/thehiker/LanguageIntegrationTest.kt
+++ app/src/androidTest/java/com/team28/thehiker/LanguageIntegrationTest.kt
@@ -1,5 +1,9 @@
 package com.team28.thehiker
 
+import android.app.ActivityManager
+import android.content.Context
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -9,10 +13,12 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.team28.thehiker.SharedPreferenceHandler.SharedPreferenceHandler
+import com.team28.thehiker.language.LanguageSelector
 import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.lang.StringBuilder
 
 @RunWith(AndroidJUnit4::class)
 class LanguageIntegrationTest {
@@ -30,6 +36,28 @@ class LanguageIntegrationTest {
         switchLanguage(R.string.english, "en", "en-EN")
     }
 
+    @Test
+    fun checkLanguageSwitchPersistsAppRestart(){
+        switchLanguage(R.string.russian, "ru", "ru-RU")
+
+        //close the app
+        activityRule.scenario.onActivity {
+            //mocking an app reset this way since we didn't find a better way to fully kill the app during testing
+            //this just updates the current configuration but not the shared preferences we want to test
+            LanguageSelector.setLocaleToEnglish(it)
+        }
+        activityRule.scenario.close()
+
+
+        // In the SplashscreenActivity the app should reload the shared preferences and update the locale config
+        val scenario = ActivityScenario.launch(SplashscreenActivity::class.java)
+        scenario.onActivity {
+            val sharedPreferenceHandler = SharedPreferenceHandler()
+            Assert.assertEquals("ru", sharedPreferenceHandler.getLocalizationString(it))
+            Assert.assertEquals("ru-RU", it.resources.configuration.locales[0].toLanguageTag())
+        }
+    }
+
     fun switchLanguage(languageStringID : Int, sharedPreferenceTag : String, languageTag : String) {
         onView(withId(R.id.drawerLayout)).perform(DrawerActions.open())
         onView(withId(R.id.language)).perform(ViewActions.click())
@@ -39,8 +67,8 @@ class LanguageIntegrationTest {
         lateinit var findMeLabel : String
         activityRule.scenario.onActivity {
             val sharedPreferenceHandler = SharedPreferenceHandler()
-            Assert.assertEquals(sharedPreferenceHandler.getLocalizationString(it), sharedPreferenceTag)
-            Assert.assertEquals(it.resources.configuration.locales[0].toLanguageTag(), languageTag)
+            Assert.assertEquals(sharedPreferenceTag, sharedPreferenceHandler.getLocalizationString(it))
+            Assert.assertEquals(languageTag, it.resources.configuration.locales[0].toLanguageTag())
             altitudeLabel = it.resources.getString(R.string.altitude)
             findMeLabel = it.resources.getString(R.string.find_me)
         }
