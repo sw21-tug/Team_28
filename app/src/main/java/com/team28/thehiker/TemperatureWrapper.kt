70b724fe6fa37e95cbf8602e6598fa1082f707ae
chairete <johannes.leeb@student.tugraz.at>
2021-05-15 11:45:51 +0200

    HIKER-5 implemented passing register Listener test [LJ, LT]

--- app/src/main/java/com/team28/thehiker/TemperatureWrapper.kt
+++ app/src/main/java/com/team28/thehiker/TemperatureWrapper.kt
@@ -8,12 +8,20 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.util.Log
 
-open class TemperatureWrapper(private var sensorManager: SensorManager){
+open class TemperatureWrapper(private var sensorManager: SensorManager): SensorEventListener{
 
     private val temperatureSensor : Sensor?
 
+
     init {
         temperatureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+        if (temperatureSensor != null) {
+            sensorManager.registerListener(this, temperatureSensor, 1000);
+        }
+    }
+
+    fun kill(){
+        sensorManager.unregisterListener(this)
     }
 
     open fun isTemperatureSensorAvailable() : Boolean{
@@ -23,5 +31,12 @@ open class TemperatureWrapper(private var sensorManager: SensorManager){
 
     open fun getTemperature() : Double?{
         return null
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
     }
 }
