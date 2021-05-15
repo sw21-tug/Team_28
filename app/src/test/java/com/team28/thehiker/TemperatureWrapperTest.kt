bfa3d58e4dd202a7893ed3b49b21a27fe0f06a7f
chairete <johannes.leeb@student.tugraz.at>
2021-05-15 11:59:11 +0200

    HIKER-5 add Testcasse to check for correct sensor event handling [LJ, LT]

--- app/src/test/java/com/team28/thehiker/TemperatureWrapperTest.kt
+++ app/src/test/java/com/team28/thehiker/TemperatureWrapperTest.kt
@@ -1,6 +1,7 @@
 package com.team28.thehiker
 
 import android.hardware.Sensor
+import android.hardware.SensorEvent
 import android.hardware.SensorManager
 import org.junit.Assert.*
 import org.junit.Test
@@ -9,6 +10,8 @@ import org.mockito.Mockito.*
 
 class TemperatureWrapperTest {
 
+    private val FLOAT_DELTA = 0.0001f
+    private val TEMPERATURE_TEST_VARIABLE = 22.4f
     @Test
     fun testIsTemperatureSensorAvailableFalse(){
         val sensorManagerTempUnavailable : SensorManager = mock(SensorManager::class.java)
@@ -40,5 +43,28 @@ class TemperatureWrapperTest {
         verify(sensorManager, after(2000).atLeast(1)).registerListener(temperatureWrapper, temperatureSensor, 1000)
     }
 
+    @Test
+    fun testUpdatesInternalVariable(){
+        val sensorManager : SensorManager = mock(SensorManager::class.java)
+        val temperatureWrapper = TemperatureWrapper(sensorManager)
+        //no need to disable listener as no listener will be
+        // registered since the SensorManager returns null
 
+        val sensorEvent = mockSensorEvent(22.4f)
+        temperatureWrapper.onSensorChanged(sensorEvent)
+
+        assertTrue((TEMPERATURE_TEST_VARIABLE + FLOAT_DELTA) >= temperatureWrapper.getTemperature()!!)
+        assertTrue((TEMPERATURE_TEST_VARIABLE - FLOAT_DELTA) <= temperatureWrapper.getTemperature()!!)
+    }
+
+    fun mockSensorEvent(temperature : Float) : SensorEvent{
+        val sensorEvent : SensorEvent = mock(SensorEvent::class.java)
+        val field = SensorEvent::class.java.getField("values")
+        field.isAccessible = true
+
+        val floatArray = FloatArray(1)
+        floatArray[0] = temperature
+        field.set(sensorEvent, floatArray)
+        return sensorEvent
+    }
 }
