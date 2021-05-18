c3646f6c5e28fd5c3948f0f4af4becc2f179a9a2
Markus152 <markus.watko@student.tugraz.at>
2021-05-18 15:31:17 +0200

    HIKER-46 added dependecies for permission checking in espresso (no nativ support) [NJ,WM]

--- app/src/androidTest/java/com/team28/thehiker/PermissionHandlingInstrumentedTest.kt
+++ app/src/androidTest/java/com/team28/thehiker/PermissionHandlingInstrumentedTest.kt
@@ -1,11 +1,16 @@
 package com.team28.thehiker
 
-import androidx.test.espresso.Espresso.*
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isClickable
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,13 +20,30 @@ import org.junit.runner.RunWith
 class PermissionHandlingInstrumentedTest {
 
     @get:Rule
-    var activityRule = ActivityScenarioRule(AltitudeActivity::class.java)
+    var activityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule<MainActivity>(MainActivity::class.java)
+
+    fun grantPermission() {
+        val instrumentation = InstrumentationRegistry.getInstrumentation()
+        val allowPermission = UiDevice.getInstance(instrumentation).findObject(UiSelector().text("Allow"))
+        if(allowPermission.exists())
+            allowPermission.click()
+    }
+
+    fun denyPermission() {
+        val instrumentation = InstrumentationRegistry.getInstrumentation()
+        val denyPermission = UiDevice.getInstance(instrumentation).findObject(UiSelector().text("Deny"))
+        if(denyPermission.exists())
+            denyPermission.click()
+    }
 
     @Test
-    fun testValueUpdateWithZero(){
-
+    fun a_testMainScreenWithoutPermission(){
+        denyPermission()
+        onView(withId(R.id.btn_altitude))
+                .check(ViewAssertions.matches(ViewMatchers.withText("Altitude")))
         return
     }
+
 
 
 
