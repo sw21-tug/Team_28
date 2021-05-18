3f43981b1950ad870110062fe0a7efb8e9d4d2de
Johannes Leeb <johannes.leeb@student.tugraz.at>
2021-05-18 17:22:35 +0200

    HIKER-26 fixed testcase according to string format and imports [LJ, SN]

--- app/src/androidTest/java/com/team28/thehiker/HumidityUITest.kt
+++ app/src/androidTest/java/com/team28/thehiker/HumidityUITest.kt
@@ -1,10 +1,6 @@
 package com.team28.thehiker
 
 import android.hardware.SensorEvent
-import android.hardware.SensorManager
-import android.widget.TextView
-import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
@@ -16,16 +12,18 @@ import org.mockito.Mockito
 
 class HumidityUITest {
 
+    val HUMDITY_TEST_VALUE = 50.2f
+
     @get:Rule
     var activityRule = ActivityScenarioRule(HumidityActivity::class.java)
 
     @Test
     fun testUpdateHumidity(){
-        val sensorEvent = mockSensorEvent(50.2f)
+        val sensorEvent = mockSensorEvent(HUMDITY_TEST_VALUE)
 
         activityRule.scenario.onActivity { it.onSensorChanged(sensorEvent) }
         onView(withId(R.id.humidity))
-                .check(ViewAssertions.matches(ViewMatchers.withText("50.2")))
+                .check(ViewAssertions.matches(ViewMatchers.withText("50.2 %")))
     }
 
     fun mockSensorEvent(humidity : Float) : SensorEvent{
