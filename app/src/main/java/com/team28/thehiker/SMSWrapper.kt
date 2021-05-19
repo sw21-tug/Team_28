212cefd4cc5ebb85ba42af615f392501e9a908ce
Sebastian Grief <sebastian.grief@student.tugraz.at>
2021-05-19 16:30:59 +0200

    HIKER-54: Implement SMSWrapper [GS, LT]

--- app/src/main/java/com/team28/thehiker/SMSWrapper.kt
+++ app/src/main/java/com/team28/thehiker/SMSWrapper.kt
@@ -3,11 +3,27 @@ package com.team28.thehiker
 import android.telephony.SmsManager
 
 class SMSWrapper (val smsManager : SmsManager, val delayMS : Long){
-    fun start(){
 
+    private var thread : Thread
+    private var message = "This is a placeholder message: [Chuckles] I'm in danger"
+    private var messageFineAgain = "This is a placeholder message: [Chuckles] I'm not in danger anymore"
+    private var stopAlarm = false
+
+    init {
+        thread = Thread(Runnable {
+            while (!stopAlarm) {
+                smsManager.sendTextMessage("+436643556189", null, message, null, null)
+                Thread.sleep(delayMS)
+            }
+        })
     }
 
-    fun stop(){
+    fun start() {
+        thread.start()
+    }
 
+    fun stop() {
+        stopAlarm = true
+        smsManager.sendTextMessage("+436643556189", null, messageFineAgain, null, null)
     }
 }
--- app/src/test/java/com/team28/thehiker/SMSWrapperTest.kt
+++ app/src/test/java/com/team28/thehiker/SMSWrapperTest.kt
@@ -1,6 +1,7 @@
 package com.team28.thehiker
 
 import android.telephony.SmsManager
+import com.team28.thehiker.Constants.Constants
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito.*
@@ -13,18 +14,16 @@ class SMSWrapperTest {
     var smsMock = mock(SmsManager::class.java);
 
     @Test
-    fun testStartStopIntervall(){
+    fun testStartStopInterval(){
         val smsWrapper = SMSWrapper(smsMock, UPDATE_INTERVAL);
 
         verify(smsMock, never()).sendTextMessage(anyString(), anyString(), anyString(), any(), any())
 
         smsWrapper.start()
-        verify(smsMock, times(1)).sendTextMessage(anyString(), anyString(), anyString(), any(), any())
-        verify(smsMock, after(3 * UPDATE_INTERVAL).atLeast(2)).sendTextMessage(anyString(), anyString(), anyString(), any(), any())
+        verify(smsMock, after(3 * UPDATE_INTERVAL).atLeast(3)).sendTextMessage(anyString(), any(), anyString(), any(), any())
 
         smsWrapper.stop()
-        verify(smsMock, after(2 * UPDATE_INTERVAL).never()).sendTextMessage(anyString(), anyString(), anyString(), any(), any())
-
+        verify(smsMock, after(2 * UPDATE_INTERVAL).atMost(5)).sendTextMessage(anyString(), any(), anyString(), any(), any())
     }
     
 }
