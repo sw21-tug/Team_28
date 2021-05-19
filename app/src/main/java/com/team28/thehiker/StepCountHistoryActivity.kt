523bf0a71b3f94625ad7c69f47ae69a2401ee2ea
Jaime993155 <33482887+Jaime993155@users.noreply.github.com>
2021-05-19 16:31:13 +0200

    HIKER-14 list view for step history [FL,NJ]

--- app/src/main/java/com/team28/thehiker/StepCountHistoryActivity.kt
+++ app/src/main/java/com/team28/thehiker/StepCountHistoryActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.PopupMenu
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.navigation.NavigationView
@@ -22,5 +24,13 @@ class StepCountHistoryActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_step_history)
         //load saved values
+
+        val listView = findViewById<ListView>(R.id.list_view_left)
+        val names = arrayOf("date_1","date_2","date_3")
+
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, names)
+
+        listView.adapter=arrayAdapter
     }
 }
