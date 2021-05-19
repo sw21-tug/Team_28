7c912de59073b6ab4a7e7f19f3f7ab5613745014
Sebastian Grief <sebastian.grief@student.tugraz.at>
2021-05-19 17:26:01 +0200

    HIKER-54: added location [GS, LT]

--- app/src/main/java/com/team28/thehiker/SMSWrapper.kt
+++ app/src/main/java/com/team28/thehiker/SMSWrapper.kt
@@ -8,31 +8,36 @@ class SMSWrapper(val smsManager: SmsManager, val delayMS: Long, val numbers: Lis
     private var thread : Thread
     private val message = "This is a placeholder message: [Chuckles] I'm in danger"
     private val messageFineAgain = "This is a placeholder message: [Chuckles] I'm not in danger anymore"
+    private lateinit var location : Location
+
     private var stopAlarm = false
+    private var threadRunning = false
 
     init {
         thread = Thread(Runnable {
             while (!stopAlarm) {
                 numbers.forEach {
-                    smsManager.sendTextMessage(it, null, message, null, null)
-                };
+                    smsManager.sendTextMessage(it, null, "$message $location", null, null)
+                }
                 Thread.sleep(delayMS)
             }
         })
     }
 
-    fun start() {
-        thread.start()
-    }
-
     fun stop() {
         stopAlarm = true
         numbers.forEach {
-            smsManager.sendTextMessage(it, null, message, null, null)
-        };
+            smsManager.sendTextMessage(it, null, messageFineAgain, null, null)
+        }
     }
 
-    override fun notifyLocationUpdate(location: Location) {}
+    override fun notifyLocationUpdate(location: Location) {
+        this.location = location
+        if (!threadRunning) {
+            thread.start()
+            threadRunning = true
+        }
+    }
 
     fun getMessage() : String{
         return message
