17abb5f6f0cb34db390552cea35677084943a2a1
TamaraSchweiger <tschweig@htl-steyr.ac.at>
2021-05-19 14:30:35 +0200

    HIKER-7 add testcase for updateSpeed() and implement basic layout [LJ, ST]

---
+++ app/src/androidTest/java/com/team28/thehiker/SpeedUITest.kt
@@ -0,0 +1,31 @@
+package com.team28.thehiker
+
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class SpeedUITest {
+
+    @get:Rule
+    var activityRule = ActivityScenarioRule(SpeedActivity::class.java)
+
+    @Test
+    fun testValueUpdateWithZero(){
+        activityRule.scenario.onActivity { it.updateSpeed(0.0) }
+        onView(withId(R.id.speed)).check(matches(withText("0.00 km/h")))
+    }
+
+    @Test
+    fun testValueUpdateWithPositiveNumber(){
+        activityRule.scenario.onActivity { it.updateSpeed(6.0) }
+        onView(withId(R.id.altitude)).check(matches(withText("6.00 km/h")))
+    }
+}
---
+++ app/src/main/java/com/team28/thehiker/SpeedActivity.kt
@@ -0,0 +1,15 @@
+package com.team28.thehiker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SpeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speed)
+    }
+
+    fun updateSpeed(speed : Double){
+
+    }
+}
